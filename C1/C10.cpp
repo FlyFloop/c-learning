f42d84eda273e2da9eb9d941d2d4f40999c53380
Alper <alperyorgun2@gmail.com>
2020-05-09 03:55:24 +0300

    arr
    1

--- C1/C10.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdlib.h>
-#include<locale.h>
-#include<stdio.h>
-
-main(){
-	setlocale(LC_ALL,"Turkish");
-	
-}
--- C1/C16.cpp
+++
@@ -1,7 +0,0 @@
-#include<locale.h>
-#include<stdio.h>
-#include<stdlib.h>
-// diziler
-int main(){
-	setlocale(LC_ALL,"Turkish");
-}
