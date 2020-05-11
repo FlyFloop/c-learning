4aa01a7cf54f280b840b603958dd586011b4838a
Alper <alperyorgun2@gmail.com>
2020-05-11 19:33:03 +0300

    Update if else elseif exercises.cpp

--- codeforwin.com exercises/if else elseif exercises.cpp
+++ codeforwin.com exercises/if else elseif exercises.cpp
@@ -1,9 +1,270 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 //link:https://codeforwin.org/2015/05/if-else-programming-practice.html
 main(){
 	
 	
+	//not done
+	//19-)Write a C program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+	int fizik,kimya,biyo,mat,bilgi,ortalama;
+	printf("5 dersten aldiginiz notlari sirasiyla giriniz:");
+	scanf("%d%d%d%d%d",&fizik,&kimya,&biyo,&mat,&bilgi);
+	ortalama=(fizik+kimya+biyo+mat+bilgi)/5;
+	if(ortalama>=90){
+		printf("harfnotunuz:A");
+	}
+	else if(ortalama>=80){
+		printf("harfnotunuz:B");
+	}
+	else if(ortalama>=70){
+		printf("harfnotunuz:C");
+	}
+	else if(ortalama>=60){
+		printf("harfnotunuz:D");
+	}
+	else if(ortalama>=40){
+		printf("harfnotunuz:E");
+	}
+	else if(ortalama<40){
+		printf("harfnotunuz:F");
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	//18-)Write a C program to calculate profit or loss.
+	int giris,cikis,net;
+	printf("giris fiyatini soyleyiniz:");
+	scanf("%d",&giris);
+	printf("cikis fiyatini soyleyiniz:");
+	scanf("%d",&cikis);
+	net=cikis-giris;
+	if(net>0){
+		printf("kardasiniz");
+		
+	}
+	else if(net==0){
+		printf("ne kardasýnýz ne zararda");
+	}
+	else{
+		printf("zarardasýnýz");
+	}
+	
+	
+	
+	/*
+	//17-)Write a C program to find all roots of a quadratic equation.
+	float a,b,c,kok1,kok2,dis;
+	printf("denklemin sirasiyla a,b,c degerlerini giriniz:");
+	scanf("%f%f%f",&a,&b,&c);
+	dis=(b*b)-(4*a*c);
+	if(dis>0){
+		kok1=((-b)+ sqrt(dis))/(2*a);
+		kok2=((-b)+- sqrt(dis))/(2*a);
+		printf("kokler sirasiyla\n1.kok:%f\n2.kok:%f",kok1,kok2);
+	}
+	else if(dis==0){
+		kok1=(-b)/(2*a);
+		kok2=kok1;
+		printf("kokler sirasiyla\n1.kok:%f\n2.kok:%f",kok1,kok2);
+	}
+	else{
+		printf("gercek sayilarda koku yoktur");
+	}
+	
+	
+	
+	
+	
+	/*
+	//16-)Write a C program to check whether the triangle is equilateral, isosceles or scalene triangle.
+	int aci1,aci2,aci3;
+	printf("ucgenin 3 acisini giriniz:");
+	scanf("%d%d%d",&aci1,&aci2,&aci3);
+	if(aci1==90 || aci2==90||aci3==90){
+		printf("girdiginiz ucgen dik ucgendir");
+	}
+	else if (aci1==aci2==aci3){
+		printf("eskenar ucgendir");
+	}
+	else if(aci1==aci2){
+		printf("ikizkenar ucgendir");
+	}
+	else if(aci1==aci3){
+		printf("ikizkenar ucgendir");
+	}
+	else if(aci2==aci3){
+		printf("ikizkenar ucgendir");
+	}
+	else{
+		printf("dik ucgen veya ikizkenar ucgen degildir");
+	}
+	
+	
+	
+	
+	/*
+	//15-)Write a C program to input all sides of a triangle and check whether triangle is valid or not.
+	int k1,k2,k3;
+	printf("ucgenin sirasiyla ilk  3 kenarini giriniz:");
+	scanf("%d%d%d",&k1,&k2,&k3);
+	if((k1+k2>k3) && (k1+k3>k2) &&(k2+k3>k1)){
+		printf("ucgen olusur");
+	}
+	else{
+		printf("ucgen olusmaz")
+	}
+	
+	
+	
+	
+	/*
+	//14-)Write a C program to input angles of a triangle and check whether triangle is valid or not.
+	int aci1,aci2,aci3;
+	printf("ucgenin 3 acisini giriniz:");
+	scanf("%d%d%d",&aci1,&aci2,&aci3);
+	if(aci1==90 || aci2==90||aci3==90){
+		printf("girdiginiz ucgen dik ucgendir");
+	}
+	else{
+		printf("dik ucgen degildir");
+	}
+	
+	
+	
+	/*
+	//13-)Write a C program to count total number of notes in given amount.
+	int sayi,sayi500,sayi100,sayi50,sayi20,sayi10,sayi5,sayi2,sayi1;
+	printf("bir sayi giriniz:");
+	scanf("%d",&sayi);
+	 if(sayi >= 500)
+    {
+        sayi500 = sayi/500;
+        sayi -= sayi500 * 500;
+    }
+    if(sayi >= 100)
+    {
+          sayi100 = sayi/100;
+        sayi -= sayi500 * 100;
+    }
+    if(sayi >= 50)
+    {
+        sayi50 = sayi/50;
+        sayi -= sayi50 * 50;
+    }
+    if(sayi >= 20)
+    {
+           sayi20 = sayi/20;
+        sayi -= sayi20 * 20;
+    }
+    if(sayi >= 10)
+    {
+        sayi10 = sayi/10;
+        sayi -= sayi10 * 10;
+    }
+    if(sayi >= 5)
+    {
+     sayi5 = sayi/5;
+        sayi -= sayi5 * 5;
+    }
+    if(sayi >= 2)
+    {
+            sayi2 = sayi/2;
+        sayi -= sayi2 * 2;
+    }
+    if(amount >= 1)
+    {
+       sayi1 = sayi;
+    }
+
+	
+	
+	
+	
+	/*
+	//12-)Write a C program to input month number and print number of days in that month.
+	//2019 calender
+	int ay;
+	printf("herhangi bir ayin numarasini giriniz:");
+	scanf("%d",&ay);
+	if(ay==1){
+		printf("ocak 31 gündür");
+	}
+	if(ay==2){
+		printf("subat 29 gündür");
+	}
+	if(ay==3){
+		printf("mart 31 gündür");
+	}
+	if(ay==4){
+		printf("nisan 30 gündür");
+	}
+	if(ay==5){
+		printf("mayis 31 gündür");
+	}
+	if(ay==6){
+		printf("haziran 30 gündür");
+	}
+	if(ay==7){
+		printf("temmuz 31 gündür");
+	}
+	if(ay==8){
+		printf("agustos 31 gündür");
+	}
+	if(ay==9){
+		printf("eylul 30 gündür");
+	}
+	if(ay==10){
+		printf("ekim 31 gündür");
+	}
+	if(ay==11){
+		printf("kasim 30 gündür");
+	}
+	if(ay==12){
+		printf("aralýk 31 gündür");
+	}
+	
+	
+	
+	
+	
+	/*
+	//11-)Write a C program to input week number and print week day.
+	int gun;
+	printf("1-7 arasinda bir gunun sayisini giriniz:");
+	scanf("%d",&gun);
+	if(gun==1){
+		printf("pazartesi");
+	}
+	if(gun==2){
+		printf("sali");
+	}
+	if(gun==3){
+		printf("carsamba");
+	}
+	if(gun==4){
+		printf("persembe");
+	}
+	if(gun==5){
+		printf("cuma");
+	}
+	if(gun==6){
+		printf("cumartesi");
+	}
+	if(gun==7){
+		printf("pazar");
+	}
+	
+	
+	
+	
+	
+	/*
 	//10-)Write a C program to check whether a character is uppercase or lowercase alphabet.
 	char karakter;
 	printf("bir karakter giriniz:");
